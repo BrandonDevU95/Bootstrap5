600a43e6e920f1a3bca7a577059cdef9e00ba0f2
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-06-19 22:27:32 -0500

    84 - Componente toast

--- 05-formulario/app.js
+++ 05-formulario/app.js
@@ -3,6 +3,7 @@ console.log('Funcionando');
 const formulario = document.querySelector('#formulario');
 const btnEnviar = document.querySelector('#btnEnviar');
 const btnCargando = document.querySelector('#btnCargando');
+const toast = document.querySelector('.toast');
 
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
@@ -16,6 +17,8 @@ formulario.addEventListener('submit', (e) => {
 	window.setTimeout(() => {
 		btnEnviar.classList.remove('d-none');
 		btnCargando.classList.add('d-none');
+		const evento = new bootstrap.Toast(toast);
+		evento.show();
 	}, 3000);
 	formulario.reset();
 });
