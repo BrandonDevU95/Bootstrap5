9ecc281ec6a1c8659ffd4f46fb07988d7d1571d7
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-06-19 22:20:42 -0500

    83 - Boton loading

--- 05-formulario/app.js
+++ 05-formulario/app.js
@@ -1,6 +1,8 @@
 console.log('Funcionando');
 
 const formulario = document.querySelector('#formulario');
+const btnEnviar = document.querySelector('#btnEnviar');
+const btnCargando = document.querySelector('#btnCargando');
 
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
@@ -8,5 +10,12 @@ formulario.addEventListener('submit', (e) => {
 	console.log('Campo Email:', data.get('email'));
 	console.log('Campo Password:', data.get('password'));
 	console.log('Campo Checkbox:', data.get('checkbox'));
+
+	btnEnviar.classList.add('d-none');
+	btnCargando.classList.remove('d-none');
+	window.setTimeout(() => {
+		btnEnviar.classList.remove('d-none');
+		btnCargando.classList.add('d-none');
+	}, 3000);
 	formulario.reset();
 });
