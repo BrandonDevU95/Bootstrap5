8179d1c91b11dfdb84bf31addbfcfebfa0f9b5c0
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-06-19 22:03:59 -0500

    81 - Leer input con Javascript

--- 05-formulario/app.js
+++ 05-formulario/app.js
@@ -1,6 +1,11 @@
 console.log('Funcionando');
 
 const formulario = document.querySelector('#formulario');
+
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
+	const data = new FormData(formulario);
+	console.log('Campo Email:', data.get('email'));
+	console.log('Campo Password:', data.get('password'));
+	formulario.reset();
 });
