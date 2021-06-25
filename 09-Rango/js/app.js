1e2272e7c433584461a70e45ac600e6efdc8758e
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-06-24 22:05:38 -0500

    117 - Detectar Form range

---
+++ 09-Rango/js/app.js
@@ -0,0 +1,7 @@
+const inputRango = document.getElementById('inputRango');
+const visitas = document.getElementById('visitas');
+const precio = document.getElementById('precio');
+
+inputRango.addEventListener('input', () => {
+	console.log('Click');
+});
