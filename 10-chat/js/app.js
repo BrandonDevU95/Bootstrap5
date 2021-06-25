5fc8a514a1759049fb5becdeb1cbc30e399c0ef5
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-06-25 00:59:17 -0500

    127 - Seleccionando Ids

---
+++ 10-chat/js/app.js
@@ -0,0 +1,6 @@
+const btnIngreso = document.querySelector('#btnIngreso');
+const btnCerrarSesion = document.querySelector('#btnCerrarSesion');
+const contenidoWeb = document.querySelector('#contenidoWeb');
+const nombreUsuario = document.querySelector('#nombreUsuario');
+const formulario = document.querySelector('#formulario');
+const texto = document.querySelector('#texto');
