c9e6da779da86007d077d8bc1b7a53caf1828ad2
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-06-25 01:07:06 -0500

    128 - Detectando Usuario

--- 10-chat/js/app.js
+++ 10-chat/js/app.js
@@ -4,3 +4,22 @@ const contenidoWeb = document.querySelector('#contenidoWeb');
 const nombreUsuario = document.querySelector('#nombreUsuario');
 const formulario = document.querySelector('#formulario');
 const texto = document.querySelector('#texto');
+
+firebase.auth().onAuthStateChanged((user) => {
+	if (user) {
+		accionCerrarSesion();
+	} else {
+		accionAcceder();
+	}
+});
+
+const accionAcceder = () => {
+	console.log('Sin Usuario');
+	formulario.classList.add('d-none');
+	contenidoWeb.innerHTML = `<p class="lead mt-5 text-center">Debes iniciar sesión</p>`;
+};
+
+const accionCerrarSesion = () => {
+	console.log('Usuario');
+	formulario.classList.remove('d-none');
+};
