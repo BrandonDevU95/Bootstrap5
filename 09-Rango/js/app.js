043f17036263cf948112ba0b1aa3d135eadb4e5a
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-06-24 22:11:45 -0500

    119 - Views Dinamicos

--- 09-Rango/js/app.js
+++ 09-Rango/js/app.js
@@ -1,7 +1,9 @@
 const inputRango = document.getElementById('inputRango');
 const visitas = document.getElementById('visitas');
 const precio = document.getElementById('precio');
+const arrayVisitas = ['10K', '50K', '100K', '500K', '1M'];
 
 inputRango.addEventListener('input', () => {
 	precio.textContent = inputRango.value;
+	visitas.textContent = arrayVisitas[inputRango.value / 8 - 1];
 });
